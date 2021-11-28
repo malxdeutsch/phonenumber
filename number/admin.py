3f9cf7b63507011c107b3d89f7b66148d5f200ce
Cameron Isen <malxdeutsch@gmail.com>
2021-11-28 14:26:17 +0200

    add admin

--- number/admin.py
+++ number/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Person, PhoneNumberField
+from phonenumber_field.widgets import PhoneNumberInternationalFallbackWidget
 
 # Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        PhoneNumberField: {'widget': PhoneNumberInternationalFallbackWidget}
+    }
+admin.site.register(Person, PersonAdmin)
+
